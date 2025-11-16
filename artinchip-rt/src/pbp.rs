555e78872d73a1ea58d1ff2534b72efabdfa145d
Zhouqi Jiang <jiangzhouqi25@mails.ucas.ac.cn>
2025-11-16 18:37:44 +0800

    fix(rt): initialize T-Head and RISC-V floating point in PBP start function
    Enables D-cache and I-cache to improve execution speed. Implements platform specific halt operation.
    
    Signed-off-by: Zhouqi Jiang <jiangzhouqi25@mails.ucas.ac.cn>

--- artinchip-rt/src/pbp.rs
+++ artinchip-rt/src/pbp.rs
@@ -29,7 +29,29 @@ pub extern "C" fn _start() {
     static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
 
     naked_asm!(
-        // Clear `.bss` section
+        // 1. Disable interrupt
+        "   csrw    mie, zero",
+        // 2. Hart specific initialization
+        // Enable T-Head instruction sets (THEADISAEE) and
+        // misaligned access (MM) in `mxstatus` register.
+        "   li      t1, 0x408000
+            csrs    0x7c0, t1",
+        // TODO SPUSHEN and SPSWAPEN in `mexstatus` once we have trap handler
+        // Enable T-Head caches (BTB=1, BPE=1, RS=1, WA=1, WB=1, DE=1, IE=1)
+        // in `mhcr` register;
+        // enable T-Head hint operations (PREF_N=3, AMR=1, D_PLD=1)
+        // in `mhint` register.
+        "   li      t0, 0x103f
+            csrw    0x7c1, t0
+            li      t1, 0x600c
+            csrw    0x7c5, t1",
+        // 3. Initialize float point unit
+        "   li      t0, 0x4000
+            li      t1, 0x2000
+            csrc    mstatus, t0
+            csrs    mstatus, t1
+            csrw    fcsr, zero",
+        // 4. Clear `.bss` section
         "   la      t0, sbss
             la      t1, ebss
         2:  bgeu    t0, t1, 3f
@@ -37,13 +59,18 @@ pub extern "C" fn _start() {
             addi    t0, t0, 4
             j       2b",
         "3:",
-        // Prepare programming language stack
+        // 5. Prepare programming language stack
         "   la      sp, {stack} + {stack_size}",
-        // Start Rust main function
+        // 6. Start Rust main function
         "   j       {main}",
-        // Platform halt if main function returns
-        "4: wfi
-        j       4b",
+        // 7. Platform halt (by loop-wfi) if main function returns
+        // Set T-Head wfi behavior to deep-sleep, disable interrupt then
+        // loop-wfi. Clears LPMD=0 and WFEEN=0 in `mexstatus`.
+        "   li      t0, 0x1c
+            csrc    0x7e1, t0
+            csrci   mstatus, 0x8
+        2:  wfi
+            j       2b",
         stack_size = const STACK_SIZE,
         stack      =   sym STACK,
         main       =   sym pbp_main,
